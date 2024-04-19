3c231dc5a75799c2c0b1a1eb78c00ae3fc3b6fac
Elianamos <elianamos29@gmail.com>
2024-04-19 21:01:10 +0300

    optimize rotate array

--- rotate_array.py
+++ rotate_array.py
@@ -1,14 +1,27 @@
 from typing import List
 
 
+def reverse(nums, start, end):
+    while start < end:
+        nums[start], nums[end] = nums[end], nums[start]
+        start += 1
+        end -= 1
+
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
         l = len(nums)
-        for i in range(k):
-            arr = nums.copy()
-            for j in range(l - 1):
-                nums[j + 1] = arr[j]
-            nums[0] = arr[l-1]
+        k = k % l
+        
+        reverse(nums, 0, l - 1)
+        reverse(nums, 0, k - 1)
+        reverse(nums, k, l - 1)
+
+if __name__ == '__main__':
+    s = Solution()
+    nums = [1, 2, 3, 4, 5, 6, 7]
+    k = 3
+    s.rotate(nums, k)
+    print(nums)
