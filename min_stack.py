c941e83b63eb7a155394b6a10aceec8d8de81625
Elianamos <elianamos29@gmail.com>
2024-11-19 13:17:37 +0300

    update min stack: getMin from O(n) to O(1)

--- min_stack.py
+++ min_stack.py
@@ -2,18 +2,27 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
+        self.min_stack = []
 
     def push(self, val: int) -> None:
         self.stack.append(val)
 
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
     def pop(self) -> None:
-        self.stack.pop()
+        if self.stack:
+            val = self.stack.pop()
+            if val == self.min_stack[-1]:
+                self.min_stack.pop()
 
     def top(self) -> int:
-        return self.stack[-1]
+        if self.stack:
+            return self.stack[-1]
 
     def getMin(self) -> int:
-        return min(self.stack)
+        if self.min_stack:
+            return self.min_stack[-1]
 
 if __name__ == '__main__':
     s = MinStack()
