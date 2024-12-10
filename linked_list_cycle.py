377329e6b28f66af46646dcbbf33e61f3f9f041d
Elianamos <elianamos29@gmail.com>
2024-12-10 21:35:41 +0300

    linked list cycle

---
+++ linked_list_cycle.py
@@ -0,0 +1,74 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        nodes_seen = set()
+        while head:
+            if head in nodes_seen:
+                return True
+            nodes_seen.add(head)
+            head = head.next
+        return False
+
+if __name__ == '__main__':
+    s = Solution()
+    head = ListNode(3)
+    head.next = ListNode(2)
+    head.next.next = ListNode(0)
+    head.next.next.next = ListNode(-4)
+    head.next.next.next.next = head.next
+    print(s.hasCycle(head))
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = head
+    print(s.hasCycle(head))
+    head = ListNode(1)
+    print(s.hasCycle(head))
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    print(s.hasCycle(head))
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = head
+    print(s.hasCycle(head))
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = head.next
+    print(s.hasCycle(head))
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = head.next.next
+    print(s.hasCycle(head))
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = head.next.next.next
+    print(s.hasCycle(head))
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = head.next.next.next
+    print(s.hasCycle(head))
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = head.next.next
+    print(s.hasCycle(head))
